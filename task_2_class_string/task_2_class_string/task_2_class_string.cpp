393c2a5065b00cae17d7bea420ba9941a65b7d1c
zark1cerut <zark.cerut@gmail.com>
2022-03-13 22:27:17 +0300

    Create task_3 without constructor and create task_4 without method

--- task_2_class_string/task_2_class_string/task_2_class_string.cpp
+++ task_2_class_string/task_2_class_string/task_2_class_string.cpp
@@ -1,3 +1,155 @@
+<<<<<<< HEAD
+﻿// task_2_class_string.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Дана строка символов. 
+//Подсчитать, сколько раз в строку входит последний символ, и заменить все пробелы на этот символ.
+
+#include <iostream>
+using namespace std;
+
+
+class String {
+
+public:
+	String() 
+	{
+		str = nullptr;
+	}
+	String(const char *str)
+	{
+
+		int length = strlen(str);
+		this->str = new char[length + 1];
+
+		for (int i = 0; i < length; i++) {
+
+			this->str[i] = str[i];
+
+		}
+
+		this->str[length] = '\0';
+
+	}
+
+
+	String& operator =(const String& other)
+	{
+		if (this->str != nullptr) delete[] str;
+
+
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+
+		for (int i = 0; i < length; i++) {
+
+			this->str[i] = other.str[i];
+
+		}
+
+		this->str[length] = '\0';
+
+
+		return *this;
+	}
+
+	String& operator +(const String& other)
+	{
+
+		String newStr;
+
+		int thisLen = strlen(this->str);
+		int otherLen = strlen(other.str);
+		newStr.str = new char[thisLen + otherLen + 1];
+
+		int i = 0;
+		for (; i < thisLen; i++)
+		{
+			newStr.str[i] = this->str[i];
+		}
+		for (int j = 0; j < otherLen; j++, i++)
+		{
+			newStr.str[i] = other.str[j];
+		}
+		newStr.str[thisLen + otherLen] = '\0'; 
+
+		return newStr;
+
+	}
+
+
+	String& lastchar(const String& other)
+	{
+		
+		if (this->str != nullptr) delete[] str;
+		int length = strlen(other.str);
+		char lastch = other.str[length - 1];
+		this->str = new char[length/2 + 1];
+		
+		int count = 0;
+
+
+		for (int i = 0; i < length; i++) {
+
+
+			this->str[i] = other.str[i];
+
+			if (other.str[i] == lastch) {
+				
+				count++;
+			}
+			if (other.str[i] == ' ') {
+
+				this->str[i] = lastch;
+
+			}
+		
+		}
+		this->str[length] = '\0';
+
+
+
+		return *this;
+	}
+
+
+
+
+	void Print() {
+
+		cout << str;
+	}
+
+	~String()
+	{
+		delete[] this->str;
+	}
+
+
+
+
+private:
+	char *str;
+
+};
+
+int main()
+{
+  
+	String str;
+	String str2("Hello world. How are you?");
+	
+	str2.Print();
+	cout << endl;
+	str.lastchar(str2);
+	
+
+	//	str.Print();
+	
+	
+
+	return 0;
+}
+
+=======
 #include <iostream>
 using namespace std;
 
@@ -150,3 +302,4 @@ int main()
 	return 0;
 }
 
+>>>>>>> 8058ca5609dd57a8417d8df26cb5744146eb7a21
