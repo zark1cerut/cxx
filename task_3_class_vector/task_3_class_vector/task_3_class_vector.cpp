30ebf1f10c976aa2dc014e33548d4f15de90a8a6
zark1cerut <zark.cerut@gmail.com>
2022-03-20 22:35:35 +0300

    UPD: make a new branch and add all tasks

---
+++ task_3_class_vector/task_3_class_vector/task_3_class_vector.cpp
@@ -0,0 +1,124 @@
+﻿
+
+#include <iostream>
+
+
+
+template <typename T>
+class Vector {
+
+private:
+    T* arr;
+    size_t sz;
+    size_t cap;
+
+    template <typename T>
+    void reserve(size_t n)
+    {
+        if (n <= cap) return;
+        newarr = reinterpret_cast<T*>(new int8_t[n * sizeof(T)]);
+        size_t i = 0;
+        try
+        {
+            for (; i < sz; ++i)
+            {
+                new(newarr + i) T(arr[i]);
+            }
+
+        }
+        catch (...)
+        {
+            for (size_t j = 0; j < i; ++j)
+            {
+                (newarr + i)->~T();
+            }
+            delete[] reinterpret_cast<int8_t*>(newarr);
+            throw;
+        }
+        for (size_t j = 0; j < sz; ++j)
+        {
+            (arr + i)->~T();
+        }
+        delete[] reinterpret_cast<int8_t*>(arr);
+        arr = newarr;
+        cap = n;
+
+    }
+
+    template <typename T>
+    void resize(size_t n, const T& value = T())
+    {
+        if (n > cap) reserve(n);
+        for (size_t i = sz; i < n; ++i)
+        {
+            new(arr + i) T(value);
+        }
+
+        if (n < sz) sz = n;
+
+
+    }
+
+    template <typename T>
+    void push_back(const T& value)
+    {
+        if (cap == sz) reserve(2 * sz);
+        new(arr + sz) T(value);
+        ++sz;
+    }
+
+    template <typename T>
+    void pop_back()
+    {
+
+        --sz;
+        (arr + sz)->~T();
+    }
+
+    template <typename T>
+    T& operator [](size_t i)
+    {
+        return arr[i];
+    }
+
+    template <typename T>
+    T& at(size_t i)
+    {
+        if (i >= sz) throw std::outofrange("...");
+        return arr[i];
+    }
+
+
+
+public:
+
+    template <typename T>
+    Vector()
+    {
+
+
+    }
+
+    template <typename T>
+    Vector(T* arr) 
+        :arr(arr)
+    {
+
+        
+    }
+    
+
+   
+};
+
+
+int main()
+{
+
+    Vector<int> array1(4);
+
+    array1.push_back(2);
+
+
+     return 0;
+}
---
+++ task_4_inheritance/task_4_inheritance/task_4_inheritance.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Shop
+{
+protected:
+
+    int rental_price;
+
+    std::string address;
+
+    
+
+
+public:
+
+
+    Shop()
+    {
+
+    }
+
+    Shop(int rental_price, std::string address)
+    {
+        this->rental_price = rental_price;
+        this->address = address;
+       
+
+    }
+
+    void Print_Sh()
+    {
+
+        std::cout << "Address: " << address << "\nRental price: " << rental_price << std::endl;
+
+    }
+
+    ~Shop()
+    {
+
+    }
+};
+
+
+class Food_Sh : public Shop
+{
+private:
+    std::string title;
+
+public:
+
+
+    Food_Sh()
+    {
+
+    }
+
+    Food_Sh(int rental_price, std::string address, std::string title)
+    {
+        this->rental_price = rental_price;
+        this->address = address;
+        this->title = title;
+    }
+
+   
+
+
+
+
+    void Print_Sh()
+    {
+        
+        std::cout << "Address: "  << address << "\nTitle: " << title << "\nRental price: " << rental_price << std::endl;
+
+    }
+
+    ~Food_Sh()
+    {
+
+    }
+
+};
+
+class Chemicals_Sh : public Shop
+{
+private:
+
+    std::string title;
+
+public:
+
+    Chemicals_Sh()
+    {
+
+    }
+
+
+
+
+
+
+    Chemicals_Sh(int rental_price, std::string address, std::string title)
+    {
+        this->rental_price = rental_price;
+        this->address = address;
+        this->title = title;
+    }
+
+
+
+
+
+
+    void Print_Sh()
+    {
+
+        std::cout << "Address: " << address << "\nTitle: " << title << "\nRental price: " << rental_price << std::endl;
+
+    }
+
+
+    ~Chemicals_Sh()
+    {
+
+    }
+
+
+};
+
+class Hypermarket_Sh : public Shop
+{
+private:
+    std::string title;
+
+public:
+
+    Hypermarket_Sh()
+    {
+
+    }
+
+
+
+
+    Hypermarket_Sh(int rental_price, std::string address, std::string title)
+    {
+        this->rental_price = rental_price;
+        this->address = address;
+        this->title = title;
+    }
+
+
+
+
+
+
+    void Print_Sh()
+    {
+
+        std::cout << "Address: " << address << "\nTitle: " << title << "\nRental price: " << rental_price << std::endl;
+
+    }
+
+    ~Hypermarket_Sh()
+    {
+
+    }
+
+
+};
+
+
+
+
+
+int main()
+{
+   
+
+    Shop s1 = Shop(21434, "ssffss");
+
+    Food_Sh f1 = Food_Sh(350, "Masherova, 4", "Diskaunter");
+
+    f1.Print_Sh();
+
+    return 0;
+}
